74a71a58cabdfc4e2689a79f16f8501504acc7d3
vkolomiyets <vyacheslav.kolomiyets@gmail.com>
2020-04-14 14:45:00 +0300

    added modal window

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { useEffect } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
-import AddTodo from "./Todo/AddTodo";
 import Loader from "./Loader";
+import Modal from "./Modal/Modal";
+
+//const AddTodo = React.lazy(() => import("./Todo/AddTodo"));
+const AddTodo = React.lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve(import("./Todo/AddTodo")), 3000);
+    })
+);
 
 function App() {
   const [todos, setTodos] = React.useState([]);
@@ -48,7 +56,10 @@ function App() {
     <Context.Provider value={{ removeTodo }}>
       <div className="wrapper">
         <h1>React Tutorial</h1>
-        <AddTodo onCreate={addTodo} />
+        <Modal />
+        <React.Suspense fallback={<p>Loading...</p>}>
+          <AddTodo onCreate={addTodo} />
+        </React.Suspense>
         {loading && <Loader />}
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo} />
